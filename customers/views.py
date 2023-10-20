49d0fecb23eca7fd3cfd68fe2caa9d5b3767ece5
bassammsmar <bassammsmar@gmail.com>
2023-10-20 19:09:45 +0300

    add annotate to customer listy

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Count
 
 
 from .models import Customers, Branch
@@ -7,7 +8,7 @@ from .models import Customers, Branch
 # Create your views here.
 class CustomersList(ListView):
     model = Branch    #context : object_list, model_list
-
+    queryset = Branch.objects.annotate(shipment_count=Count('shipments_company'))
 
 class CustomersDetail(DetailView):
     model = Customers
