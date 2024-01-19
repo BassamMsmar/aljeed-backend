96be6c93b7f1285928e7733a0a24c58f93549a15
bassammsmar <bassammsmar@gmail.com>
2024-01-19 14:42:31 +0300

    edit file requirements

--- shipments/models.py
+++ shipments/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 
 from drivers.models import Driver
 from customers.models import Customers, Branch
@@ -31,15 +33,15 @@ class Shipments(models.Model):
     user = models.ForeignKey(User,  related_name='shipments_user', on_delete=models.SET_NULL, null=True)
     driver = models.ForeignKey(Driver,  related_name='shipments_driver', on_delete=models.SET_NULL, null=True)
     customer_branch = models.ForeignKey(Branch, related_name='shipments_company', on_delete=models.SET_NULL, null=True)
-    fare = models.IntegerField()
-    days_stayed = models.IntegerField(null=True, blank=True)
-    stay_cost = models.IntegerField(null=True, blank=True)
-    deducted = models.IntegerField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS, default='Recieved')
-    destination = models.CharField(max_length=20, choices=FLAG_TYPES)
-    create_at = models.DateTimeField(default=timezone.now)
-    expected_arrival_date = models.DateTimeField(null=True, blank=True)
-    actual_delivery_date = models.DateTimeField(null=True, blank=True)
+    fare = models.IntegerField(_("Fare"),)
+    days_stayed = models.IntegerField(_("Days Stayed "), null=True, blank=True)
+    stay_cost = models.IntegerField(_("Stay Cost"), null=True, blank=True)
+    deducted = models.IntegerField(_("Deducted"), null=True, blank=True)
+    status = models.CharField(_("Status "), max_length=20, choices=STATUS, default='Recieved')
+    destination = models.CharField(_("Destination"), max_length=20, choices=FLAG_TYPES)
+    create_at = models.DateTimeField(_("Create At"), default=timezone.now)
+    expected_arrival_date = models.DateTimeField(_("Expected Arrival Date"), null=True, blank=True)
+    actual_delivery_date = models.DateTimeField(_("Actual Delivery Date "), null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.driver} - {self.destination}'
