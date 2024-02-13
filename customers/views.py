fdb5ea407e2cd5ccd59fcc00d5f62d6d44db34e3
Bassam Msmar <bassammsmar@gmail.com>
2024-02-13 12:37:38 +0300

    add customer gilters

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.db.models.aggregates import  Count
@@ -9,8 +10,13 @@ from shipments.models import Shipments
 
 # Create your views here.
 class CustomersList(ListView):
-    model = Customers    #context : object_list, model_list
+    model = Customers
+
+    
+    
 
 
 class CustomersDetail(DetailView):
     model = Customers
+
+    
